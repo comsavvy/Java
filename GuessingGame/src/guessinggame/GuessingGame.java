da90162a38269cd079ec62fc7bf2a7dfc2971ea1
comsavvy <olusolaogundepo@gmail.com>
2020-03-18 22:10:16 +0100

    Error prone code

--- GuessingGame/src/guessinggame/GuessingGame.java
+++ GuessingGame/src/guessinggame/GuessingGame.java
@@ -11,23 +11,41 @@ public class GuessingGame {
     static Scanner input = new Scanner(System.in);
     
     public static void main(String[] args) {
+       int y= 2;
+       while(y>0){
         byte guess=0;
         byte secretNo= (byte)(random()*20);
-        byte counter;
+        byte counter= 0;
         System.out.println("HELLO, I have number between 1-20\nCan you guess my number?\nBest of luck!");
+        try{
         for(byte i=0; i<5; ++i){
-            counter = (byte) ((byte)4 - i);
-            System.out.printf("Guess: ");
-            guess= input.nextByte();
-            if ( guess==secretNo){
+            int x=1;
+                counter = (byte) ((byte)4 - i);
+                System.out.printf("Guess: ");              
+                guess= input.nextByte();
+            if (guess==secretNo){
                 System.out.println("You Win!");
                 System.out.printf("Time of Guess was %d times\n", i+1);
                 break;
             }
             else if (guess > secretNo)System.out.println("Too High!");
             else if (guess < secretNo)System.out.println("Too low");
-            if ( counter != 0)System.out.printf("Time-left is %d\n", counter);
+            if ( counter != 0)System.out.printf("Time-left is %d\n", counter); 
         }
-        if (secretNo!=guess)System.out.println("You Lose");   
+        if (secretNo!=guess)System.out.println("You Lose");  
+    }    catch (Exception e){
+            System.out.print("Error!\n");
+}
+            char playAgain;
+            --y;
+            if (y != 0){
+           System.out.println("Do you want to play again[Y/N]? ");
+           playAgain= input.next().charAt(0);
+           if (playAgain=='Y' || playAgain== 'y'){
+               continue;
+           }else if (playAgain=='N' || playAgain=='n') break;
+           else System.out.println("Invalid key\n");
+            }
+ }
     }
 }
